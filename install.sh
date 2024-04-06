62af08a653626197be3d986798caaab5637d0a6c
Toni Ylenius <toni.ylenius@iki.fi>
2024-04-06 23:28:50 +0300

    install.sh, adapted from my old scripts

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+echo "This file will link files in HOME to this repo"
+
+link() {
+	local link
+	link=
+	echo -n "$2: "
+	if [ -L "$2" ] && [ $(readlink "$2") = "$1" ]
+	then
+		echo "OK"
+	else
+		rm -rf "$2"
+		mkdir -p "$(dirname $2)"
+		ln -s "$1" "$2"
+		echo "LINKED"
+	fi
+}
+
+cd "$(dirname $0)"
+DIR="$PWD"
+for file in .vimrc
+do
+	if ! [ -f "$DIR/$file" ]
+	then
+		echo "File '$file' not found from git!"
+		continue
+	fi
+	link "$DIR/$file" "$HOME/$file"
+done
+for dir in .vim/autoload .vim/doc .vim/plugin
+do
+	if ! [ -d "$DIR/$dir" ]
+	then
+		echo "Directory '$dir' not found from git!"
+		continue
+	fi
+	link "$DIR/$dir" "$HOME/$dir"
+
+done
